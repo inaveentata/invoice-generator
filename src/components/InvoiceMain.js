14f54d3f62f65fb45f14dae74a9ed2ef4c6fad42
inaveentata <inaveentata@outlook.com>
2022-08-05 10:52:14 +0530

    test for additional-f111y

--- src/components/InvoiceMain.js
+++ src/components/InvoiceMain.js
@@ -4,7 +4,7 @@ import InvoiceHeader from "./InvoiceHeader";
 import BillDetails from "./BillDetails";
 import Items from "./Items";
 import FinalAmount from "./FinalAmount";
- 
+   
 const InvoiceMain = () => {
   const [quantity, setQuantity] = useState(1)
   const [rate, setRate] = useState(0)
